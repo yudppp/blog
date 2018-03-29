b3ae900e55c04415bf7eba8a175bad08715f1f5c
yudppp <yu.d.ppp@gmail.com>
2018-03-29 20:37:12 +0900

    Update site title

--- gulpfile.js
+++ gulpfile.js
@@ -1,116 +1,111 @@
-var gulp     = require('gulp');
-var concat   = require('gulp-concat');
-var sequence = require('run-sequence').use(gulp);
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+var sequence = require("run-sequence").use(gulp);
 
 var LIB_JS_FILES = [
-  'bower_components/highlightjs/highlight.pack.js',
-  'bower_components/cheet.js/cheet.min.js'
+  "bower_components/highlightjs/highlight.pack.js",
+  "node_modules/roughjs/dist/rough.min.js",
+  "bower_components/cheet.js/cheet.min.js"
 ];
 
 var SLIDE_JS_FILES = [
-  'node_modules/talkiejs/dist/talkie.min.js',
-  'bower_components/highlightjs/highlight.pack.js'
+  "node_modules/talkiejs/dist/talkie.min.js",
+  "bower_components/highlightjs/highlight.pack.js"
 ];
 
-var APP_JS_FILES = [
-  'static/js/app.js'
-];
+var APP_JS_FILES = ["static/js/app.js"];
 
 var LIB_CSS_FILES = [
-  'node_modules/hack/dist/hack.css',
-  'node_modules/hack/dist/dark.css'
+  "node_modules/hack/dist/hack.css",
+  "node_modules/hack/dist/dark.css"
 ];
 
 var SLIDE_CSS_FILES = [
-  'node_modules/talkiejs/dist/talkie.min.css',
-  'node_modules/talkiejs/dist/talkie-default.min.css',
-  'bower_components/highlightjs/styles/monokai_sublime.css'
-]
-
-var APP_CSS_FILES = [
-  'static/css/app.css',
-  'static/css/hljs.css'
+  "node_modules/talkiejs/dist/talkie.min.css",
+  "node_modules/talkiejs/dist/talkie-default.min.css",
+  "bower_components/highlightjs/styles/monokai_sublime.css"
 ];
 
-gulp.task('watch', function () {
+var APP_CSS_FILES = ["static/css/app.css", "static/css/hljs.css"];
 
-  gulp.watch(APP_JS_FILES, function () {
-    gulp.start('js:app');
+gulp.task("watch", function() {
+  gulp.watch(APP_JS_FILES, function() {
+    gulp.start("js:app");
   });
 
-  gulp.watch(APP_CSS_FILES, function () {
-    gulp.start('css:app');
+  gulp.watch(APP_CSS_FILES, function() {
+    gulp.start("css:app");
   });
 });
 
-gulp.task('build', function () {
-  sequence('js', 'css');
+gulp.task("build", function() {
+  sequence("js", "css");
 });
 
-gulp.task('js', function () {
-  gulp.start('js:lib', 'js:slide', 'js:app');
+gulp.task("js", function() {
+  gulp.start("js:lib", "js:slide", "js:app");
 });
 
-gulp.task('css', function () {
-  gulp.start('css:lib', 'css:slide', 'css:app');
+gulp.task("css", function() {
+  gulp.start("css:lib", "css:slide", "css:app");
 });
 
-gulp.task('js:lib', function () {
-
-  gulp.src(LIB_JS_FILES)
-    .pipe(concat('lib.min.js'))
-    .pipe(gulp.dest('static/js'));
+gulp.task("js:lib", function() {
+  gulp
+    .src(LIB_JS_FILES)
+    .pipe(concat("lib.min.js"))
+    .pipe(gulp.dest("static/js"));
 });
 
-gulp.task('js:slide', function () {
-
-  gulp.src(SLIDE_JS_FILES)
-    .pipe(concat('slide.min.js'))
-    .pipe(gulp.dest('static/js'));
+gulp.task("js:slide", function() {
+  gulp
+    .src(SLIDE_JS_FILES)
+    .pipe(concat("slide.min.js"))
+    .pipe(gulp.dest("static/js"));
 });
 
-gulp.task('js:app', function () {
+gulp.task("js:app", function() {
+  var uglify = require("gulp-uglify");
 
-  var uglify = require('gulp-uglify');
-
-  gulp.src(APP_JS_FILES)
-    .pipe(concat('app.min.js'))
+  gulp
+    .src(APP_JS_FILES)
+    .pipe(concat("app.min.js"))
     .pipe(uglify())
-    .pipe(gulp.dest('static/js'));
+    .pipe(gulp.dest("static/js"));
 });
 
-gulp.task('css:lib', function () {
+gulp.task("css:lib", function() {
+  var csscomb = require("gulp-csscomb");
+  var csso = require("gulp-csso");
 
-  var csscomb = require('gulp-csscomb');
-  var csso    = require('gulp-csso');
-
-  gulp.src(LIB_CSS_FILES)
-    .pipe(concat('lib.min.css'))
+  gulp
+    .src(LIB_CSS_FILES)
+    .pipe(concat("lib.min.css"))
     .pipe(csscomb())
     .pipe(csso())
-    .pipe(gulp.dest('static/css'));
+    .pipe(gulp.dest("static/css"));
 });
 
-gulp.task('css:slide', function () {
+gulp.task("css:slide", function() {
+  var csscomb = require("gulp-csscomb");
+  var csso = require("gulp-csso");
 
-  var csscomb = require('gulp-csscomb');
-  var csso    = require('gulp-csso');
-
-  gulp.src(SLIDE_CSS_FILES)
-    .pipe(concat('slide.min.css'))
+  gulp
+    .src(SLIDE_CSS_FILES)
+    .pipe(concat("slide.min.css"))
     .pipe(csscomb())
     .pipe(csso())
-    .pipe(gulp.dest('static/css'));
+    .pipe(gulp.dest("static/css"));
 });
 
-gulp.task('css:app', function () {
+gulp.task("css:app", function() {
+  var csscomb = require("gulp-csscomb");
+  var csso = require("gulp-csso");
 
-  var csscomb = require('gulp-csscomb');
-  var csso    = require('gulp-csso');
-
-  gulp.src(APP_CSS_FILES)
-    .pipe(concat('app.min.css'))
+  gulp
+    .src(APP_CSS_FILES)
+    .pipe(concat("app.min.css"))
     .pipe(csscomb())
     .pipe(csso())
-    .pipe(gulp.dest('static/css'));
+    .pipe(gulp.dest("static/css"));
 });
--- static/js/app.js
+++ static/js/app.js
@@ -1,29 +1,49 @@
 var hostname = window.location.hostname;
-var atags = document.getElementsByTagName('a');
+var atags = document.getElementsByTagName("a");
 for (var i in atags) {
-    var tag = atags[i]
-    if (tag.href && tag.href.indexOf(hostname) == -1) {
-        tag.target = '_blank';
-    }
+  var tag = atags[i];
+  if (tag.href && tag.href.indexOf(hostname) == -1) {
+    tag.target = "_blank";
+  }
 }
 
 // hljs
 hljs.initHighlightingOnLoad();
 
 // ga
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-70167858-1', 'auto');
-ga('send', 'pageview');
+(function(i, s, o, g, r, a, m) {
+  i["GoogleAnalyticsObject"] = r;
+  (i[r] =
+    i[r] ||
+    function() {
+      (i[r].q = i[r].q || []).push(arguments);
+    }),
+    (i[r].l = 1 * new Date());
+  (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m);
+})(window, document, "script", "//www.google-analytics.com/analytics.js", "ga");
+ga("create", "UA-70167858-1", "auto");
+ga("send", "pageview");
 
-cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
-    var body = document.getElementsByTagName('body')[0]
-    var className = body.className
-    if (className === 'hack') {
-        body.className = 'hack dark'
-    } else {
-        body.className = 'hack'
-    }
+var logos = document.getElementsByClassName("logo");
+if (logos.length !== 0) {
+  var rc = rough.canvas(document.getElementsByClassName("logo")[0]);
+  rc.circle(55, 55, 80, { roughness: 0.5, fill: "#ff2e88" });
+  rc.polygon([[150, 20], [110, 90], [190, 90]], {
+    roughness: 0.5,
+    fill: "#ffd936"
+  });
+  rc.rectangle(219, 20, 70, 70, { roughness: 0.5, fill: "#2e6eff" });
+}
+
+cheet("↑ ↑ ↓ ↓ ← → ← → b a", function() {
+  var body = document.getElementsByTagName("body")[0];
+  var className = body.className;
+  if (className === "hack") {
+    body.className = "hack dark";
+  } else {
+    body.className = "hack";
+  }
 });
