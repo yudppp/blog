63fd5b88f00851e44bb963c816d545a8135c855e
yudppp <yu.d.ppp@gmail.com>
2019-09-23 17:14:46 +0900

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,4 @@
-import "../node_modules/hack/dist/hack.css";
-import "../static/app.css";
 import Head from "next/head";
-
 import Header from "./Header";
 import Footer from "./Footer";
 
@@ -9,7 +6,9 @@ const Layout: React.FC = ({ children }) => {
   return (
     <div className="hack">
       <Head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hack/0.8.1/hack.css" />
         <link rel="shortcut icon" href="/static/favicon.png" />
+        <link rel="stylesheet" href="/static/app.css" />
       </Head>
       <div className="container">
         <Header />
