38044a67d8015155bb69d413fd9a7688459ddafa
yudppp <yu.d.ppp@gmail.com>
2024-11-27 21:12:12 +0900

    remove almost code

--- features/redux/store.ts
+++
@@ -1,9 +0,0 @@
-import { createStore } from "redux";
-import { Reducer } from "./reducer";
-
-function configureStore() {
-  const store = createStore(Reducer);
-  return store;
-}
-
-export default configureStore;
